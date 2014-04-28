d716778fefa94c6cbe869e60117f81a325964f54
Kyrre Havik Eriksen <kyrre.eriksen@ciber.com>
2014-04-28 15:55:39 +0200

    An idae of how the simple server can work

--- web.rb
+++ web.rb
@@ -3,3 +3,14 @@ require 'sinatra'
 get '/' do
   "Hello, world"
 end
+
+get '/set/:name' do | 
+    # push JSON set to user
+    "Hello #{name}!"
+end
+
+get '/set/:name/:version' do | name, version |
+  # Check to see if version is lower then set's version
+  # if it is push set to user, else eror or just a blank page
+  "Hello #{name} with #{version}!"
+end
