b65912106a156bc56367730c6daa431248e84640
Kyrre Havik Eriksen <kyrre.eriksen@ciber.com>
2014-04-29 15:19:40 +0200

    Added all the sets and updated the web to use sinatra's send_file

--- web.rb
+++ web.rb
@@ -2,24 +2,20 @@ require 'sinatra'
 require 'rubygems'
 require 'git'
 require 'logger' 
-# require 'json'
+require 'json'
 
 get '/' do
   "Hello, world"
 end
 
 get '/set/:name' do | name |
-    # push JSON set to user
-    "Hello #{name}!"
+  # push JSON set to user
+  "Hello #{name}!"
+  send_file "#{name}.json"
 end
 
 get '/set/:name/:version' do | name, version |
   # Check to see if version is lower then set's version
   # if it is push set to user, else eror or just a blank page
   "Hello #{name} with #{version}!"
-end
-
-get '/update' do
-  g = Git.open("ClixEmAll", :log => Logger.new(STDOUT))
-  g.pull
 end
