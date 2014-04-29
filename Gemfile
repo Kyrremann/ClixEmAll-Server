151d4c9ae69cedc037cdcb368928b5b103e23eae
Kyrre Havik Eriksen <kyrre.eriksen@ciber.com>
2014-04-29 14:53:24 +0200

    Started working on updating the repo with json-files

--- Gemfile
+++ Gemfile
@@ -1,3 +1,6 @@
 source "https://rubygems.org"
 ruby "2.0.0"
 gem 'sinatra', '1.1.0'
+gem 'git', '1.2.6'
+gem 'json', '1.8.1'
+gem 'logger', '1.2.8'
--- web.rb
+++ web.rb
@@ -1,10 +1,14 @@
 require 'sinatra'
+require 'rubygems'
+require 'git'
+require 'logger' 
+# require 'json'
 
 get '/' do
   "Hello, world"
 end
 
-get '/set/:name' do | 
+get '/set/:name' do | name |
     # push JSON set to user
     "Hello #{name}!"
 end
@@ -13,4 +17,9 @@ get '/set/:name/:version' do | name, version |
   # Check to see if version is lower then set's version
   # if it is push set to user, else eror or just a blank page
   "Hello #{name} with #{version}!"
+end
+
+get '/update' do
+  g = Git.open("ClixEmAll", :log => Logger.new(STDOUT))
+  g.pull
 end
