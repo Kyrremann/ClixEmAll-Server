58f9d0b4a705dce785ab291d28360f9b19f247b8
Kyrre Havik Eriksen <kyrre.eriksen@ciber.com>
2014-04-30 16:20:07 +0200

    Can now retrieve set if your version is lower then current

--- web.rb
+++ web.rb
@@ -17,5 +17,16 @@ end
 get '/set/:name/:version' do | name, version |
   # Check to see if version is lower then set's version
   # if it is push set to user, else eror or just a blank page
-  "Hello #{name} with #{version}!"
+  p "#{Time.now.getutc} - Set #{name} with version #{version}"
+  jsonFile = File.read("assets/#{name}.json")
+  json = JSON.parse(jsonFile)
+  if json['version'] then
+    set_version = json['version']
+    if set_version.to_i < version.to_i then
+      redirect "/set/#{name}"
+    end
+    "No changes in set"
+  else
+    "Set does not have a version"
+  end
 end
