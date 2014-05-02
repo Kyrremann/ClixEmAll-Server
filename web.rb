5c44bc533830fa0cb75aee9f367ddff7bb4c66e7
Kyrre Havik Eriksen <kyrre.eriksen@ciber.com>
2014-05-02 17:23:18 +0200

    Added new path, which gives you a list of sets and their version

--- web.rb
+++ web.rb
@@ -8,16 +8,28 @@ get '/' do
   "Hello, world"
 end
 
+get '/set/versions' do
+  versions = "{"
+  Dir.glob("assets/*.json") do | file |
+    json = JSON.parse(File.read(file))
+    filename = File.basename("#{file}", ".json")
+    versions += "{\"set\":\"#{filename}\":{\"version\":\"#{json["version"]}\"},"
+  end
+  
+  versions = versions[0...-1]
+  return versions += "}"
+end
+
 get '/set/:name' do | name |
   # push JSON set to user
-  p "#{Time.now.getutc} - Sending file #{name}.json"
+  p "[#{Time.now.getutc}] - Sending file #{name}.json"
   send_file "assets/#{name}.json"
 end
 
 get '/set/:name/:version' do | name, version |
   # Check to see if version is lower then set's version
   # if it is push set to user, else eror or just a blank page
-  p "#{Time.now.getutc} - Set #{name} with version #{version}"
+  p "[#{Time.now.getutc}] - Set #{name} with version #{version}"
   jsonFile = File.read("assets/#{name}.json")
   json = JSON.parse(jsonFile)
   if json['version'] then
